079e47ae2298a4ddb82cdaae1f99db9c0d6ab82e
maungmatubbar <maungmatubbar@gmail.com>
2025-03-19 12:38:54 +0600

    Add Login system

--- crm/settings.py
+++ crm/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crmdb',
+        'USER': 'root',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '3360',
     }
 }
 
--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+# # Create your views here.
+def home(request):
+    #check the logged in user
+    if request.method == 'POST':    
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        # authenticate the user
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Login successful')
+            # Then we have to redirect to a page
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password')
+            return redirect('home')
+    
+    else:
+        return render(request, 'home.html',{})
 
-# Create your views here.
+# this function we will use for login
+# def login_view(request):
+#     # return render(request, 'login.html',{})
+#     pass
+
+# def logout_view(request):
+#     # return render(request, 'logout.html',{})
+#     pass
