5c3f94bfea5f9cb1a06ddeb7dba2b43bbc9f6994
maungmatubbar <maungmatubbar@gmail.com>
2025-03-22 12:04:22 +0600

    Added Client Model and show Admin Panel Client

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Client(models.Model):
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    address = models.TextField()
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=10)
+    country = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return (f"{self.full_name} {self.email} {self.created_at}")
+            
